d9cfd3957edb8958b9157da7945ff469c2d97f88
hypevhs <hypevhs@users.noreply.github.com>
2024-07-13 16:00:14 -0500

    Add --player-name-regex: filter many ProTV players

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use central::CentralCommand;
+use regex::Regex;
 use signal_hook::{consts::SIGINT, iterator::Signals};
 use std::{
+    env::args,
     sync::mpsc::{self, Sender},
     thread::{self, JoinHandle},
 };
@@ -22,6 +24,8 @@ mod vrc_log_reader_tests;
 // TODO: join and unwrap all threads
 
 fn main() {
+    let main_options = process_args();
+
     spawn_signal_handler_thread();
 
     let mut child = start_mpv();
@@ -41,9 +45,13 @@ fn main() {
     let central_tx = &central.central_tx.clone();
     spawn_mpv_ipc_threads(mpv_ipc_rx, central_tx.clone());
 
-    let lines_read_initially = central.do_initial();
+    let lines_read_initially = central.do_initial(&main_options.player_name_regex);
 
-    spawn_log_watcher_thread(central_tx.clone(), lines_read_initially);
+    spawn_log_watcher_thread(
+        central_tx.clone(),
+        lines_read_initially,
+        &main_options.player_name_regex,
+    );
 
     // this should block forever, until some kind of exit condition is met.
     central.run_central_dispatch();
@@ -55,12 +63,46 @@ fn main() {
     log_debug!("mpv process exited with code: {}", code);
 }
 
+struct MainOptions {
+    player_name_regex: Option<Regex>,
+}
+
+fn process_args() -> MainOptions {
+    let mut args = args();
+    let mut main_options = MainOptions {
+        player_name_regex: None,
+    };
+
+    args.next(); // skip argv[0]
+
+    while let Some(arg) = args.next() {
+        if arg == "--player-name-regex" {
+            if let Some(player_name_regex) = args.next() {
+                let player_name_regex = Regex::new(&player_name_regex)
+                    .expect("Invalid regex value for player-name-regex");
+                log_debug!(
+                    "Filtering log processing to only video players that match the regex: {}",
+                    player_name_regex
+                );
+                main_options.player_name_regex = Some(player_name_regex);
+            } else {
+                log_debug!("Usage: vrc-avpro-sucks [--player-name-regex <player_name_regex>]");
+                std::process::exit(1);
+            }
+        }
+    }
+
+    main_options
+}
+
 fn spawn_log_watcher_thread(
     central_tx: Sender<CentralCommand>,
     start_after_line: u64,
+    player_name_regex: &Option<Regex>,
 ) -> thread::JoinHandle<()> {
+    let player_name_regex = player_name_regex.clone();
     thread::spawn(move || {
-        let mut vlw = VrcLogWatcher::from_latest();
+        let mut vlw = VrcLogWatcher::from_latest(&player_name_regex);
         vlw.watch_file(
             start_after_line,
             |found_url| {
